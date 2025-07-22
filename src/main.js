0c3f23198983ce7bc416f5677c77489521b8799a
Pashka Antonov <pavelcreed222@gmail.com>
2025-07-22 23:12:20 +0300

    hw-11

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import { getImagesByQuery } from "./js/pixabay-api.js";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-function.js";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+const searchInput = form.querySelector("input[name='search-text']");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const query = searchInput.value.trim();
+
+  if (!query) {
+    iziToast.error({ title: "Error", message: "Please enter a search query!", position: "topRight" });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.info({
+          title: "No Results",
+          message: "Sorry, there are no images matching your search query. Please try again!",
+          position: "topRight"
+        });
+        return;
+      }
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: "Error",
+        message: "An error occurred while fetching images. Please try again later.",
+        position: "topRight"
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+});
