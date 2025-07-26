6622e9090dcf4bb2653929517a3c12b74bc5bc9d
Pashka Antonov <pavelcreed222@gmail.com>
2025-07-26 06:19:27 +0300

    hw-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,24 @@
+import axios from "axios";
+
 const API_KEY = "51425733-ebfe71f47e07541f439d053d0";
 const BASE_URL = "https://pixabay.com/api/";
 
-export function getImagesByQuery(query) {
-  const params = new URLSearchParams({
+export async function getImagesByQuery(query) {
+  const params = {
     key: API_KEY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: "true",
-  });
+  };
 
-  return fetch(`${BASE_URL}?${params}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    });
+  try {
+    const response = await axios.get(BASE_URL, { params });
+
+    return response.data.hits;
+
+  } catch (error) {
+    console.error("Помилка під час запиту зображень:", error.message);
+    throw error;
+  }
 }
